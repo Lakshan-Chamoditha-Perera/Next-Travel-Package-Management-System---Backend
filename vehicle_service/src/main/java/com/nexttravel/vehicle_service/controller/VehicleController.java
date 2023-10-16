973b302c990865154a1aac1093cc50280aad1c3a
Shan <perera.alc2000@gmail.com>
2023-10-17 00:06:47 +0530

    Vehilce Controller

--- vehicle_service/src/main/java/com/nexttravel/vehicle_service/controller/VehicleController.java
+++ vehicle_service/src/main/java/com/nexttravel/vehicle_service/controller/VehicleController.java
@@ -1,9 +1,11 @@
 package com.nexttravel.vehicle_service.controller;
 
+import com.nexttravel.vehicle_service.entity.Vehicle;
 import com.nexttravel.vehicle_service.service.VehicleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,5 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class VehicleController {
     private final VehicleService vehicleService;
+    @PostMapping(value = "/save" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?>register(
+            @RequestPart("vehicle_img1") byte[] vehicle_img1,
+            @RequestPart("vehicle_img2") byte[] vehicle_img2,
+            @RequestPart("vehicle_img3") byte[] vehicle_img3,
+            @RequestPart("vehicle_img4") byte[] vehicle_img4,
+            @RequestPart("vehicle_img5") byte[] vehicle_img5,
+            @RequestPart("vehicle") Vehicle vehicle){
+        System.out.println(vehicle);
+       return ResponseEntity.ok().build();
+    }
 
 }
