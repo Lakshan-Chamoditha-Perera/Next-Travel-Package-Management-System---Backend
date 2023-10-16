27121b865e3e444707bcc076ebbacbcbf9639a35
Shan <perera.alc2000@gmail.com>
2023-10-16 23:47:54 +0530

    vehicle controller

---
+++ vehicle_service/src/main/java/com/nexttravel/vehicle_service/controller/VehicleController.java
@@ -0,0 +1,13 @@
+package com.nexttravel.vehicle_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/vehicle")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class VehicleController {
+
+}
