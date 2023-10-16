cf997fd931bde7d4fce0f5aef394c4cbd3bf1ef9
Shan <perera.alc2000@gmail.com>
2023-10-16 23:50:49 +0530

    vehicle service added

---
+++ vehicle_service/src/main/java/com/nexttravel/vehicle_service/service/custom/VehicleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.nexttravel.vehicle_service.service.custom;
+
+import com.nexttravel.vehicle_service.service.VehicleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleServiceImpl  implements VehicleService {
+
+}
