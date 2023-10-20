09c3d887778e98832531bc03b6f9fcc4b40fe134
Shan <perera.alc2000@gmail.com>
2023-10-20 23:09:06 +0530

    delete by id

--- guide_service/src/main/java/com/nexttraverl/guide_service/service/GuideService.java
+++ guide_service/src/main/java/com/nexttraverl/guide_service/service/GuideService.java
@@ -2,4 +2,5 @@ package com.nexttraverl.guide_service.service;
 
 public interface GuideService {
     boolean existsByGuideId(String id);
+    boolean deleteGuideById(String id);
 }
--- guide_service/src/main/java/com/nexttraverl/guide_service/service/custom/GuideServiceImpl.java
+++ guide_service/src/main/java/com/nexttraverl/guide_service/service/custom/GuideServiceImpl.java
@@ -14,4 +14,16 @@ public class GuideServiceImpl implements GuideService {
     public boolean existsByGuideId(String guide_id) {
         return guideRepository.existsById(guide_id);
     }
+
+    @Override
+    public boolean deleteGuideById(String id) {
+        if (guideRepository.existsById(id)) {
+            guideRepository.deleteById(id);
+            return true;
+        }
+        throw new RuntimeException("Guide not found");
+    }
+
+
+
 }
