b01b94f60247dbf149f17d985ed7eb351ca3b713
Shan <perera.alc2000@gmail.com>
2023-10-27 15:38:15 +0530

    Eureka client : HOTEL

--- hotel_service/src/main/java/com/nexttravel/hotel_service/HotelServiceApplication.java
+++ hotel_service/src/main/java/com/nexttravel/hotel_service/HotelServiceApplication.java
@@ -2,8 +2,6 @@ package com.nexttravel.hotel_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class HotelServiceApplication {
